a7e8a8791f6bc7ab2bb5f267e4e5f3a6defd4d3b
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-07-03 15:05:54 -0400

    learned javascript loops to dynamically create images

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 
+var cities = [ "city1.jpg", "city2.jpg", "city3.jpg", "city4.jpg", "city5.jpg" ];
+
+var gallery = document.getElementById("gallery");
+
+
+
+/*
+declare counter variable, access cities array indices with for loop, print each indice to console via console.log
+*/
+var count = 0;
+var newImage = document.createElement("img");
+
+newImage.src = "images/" + cities[count];
+newImage.width = "200";
+gallery.appendChild(newImage);
+count = count + 1;
+
+
